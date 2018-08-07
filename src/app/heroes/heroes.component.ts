97b7ea5fd10919dbf984f5a224fbbdac66a8273e
archiewald <artur.kozubek1@gmail.com>
2018-08-07 15:03:50 +0200

    make hero-detail a reusable component

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -8,7 +8,6 @@ import {HEROES} from "../mock-heroes";
     templateUrl: "./heroes.component.html",
     styleUrls: ["./heroes.component.css"],
 })
-
 export class HeroesComponent implements OnInit {
     heroes = HEROES;
     selectedHero?: Hero;
