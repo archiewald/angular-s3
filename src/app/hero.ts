df88edc0a2113071d3d7adaf7e053e4598ff547b
archiewald <artur.kozubek1@gmail.com>
2018-08-07 14:07:58 +0200

    add list of heroes

--- src/app/hero.ts
+++ src/app/hero.ts
@@ -1,7 +1,4 @@
-export class Hero {
-    // ! means "definite assigment assertion"
-    // we need it since TS checks to ensure that each instance property of a class
-    // gets initialized in the constructor body, or by a property initializer
-    id!: number;
-    name!: string;
+export interface Hero {
+    id: number;
+    name: string;
 }
