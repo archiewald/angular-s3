790b9c6a01217263d7bad07a9892fb59fb09d550
archiewald <artur.kozubek1@gmail.com>
2018-08-08 14:00:16 +0200

    routing halfway

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -1,5 +1,6 @@
 import {Injectable} from "@angular/core";
 import {Observable, of} from "rxjs";
+import {delay} from "rxjs/operators";
 
 import {Hero} from "./hero";
 import {MessageService} from "./message.service";
@@ -12,8 +13,20 @@ export class HeroService {
     constructor(private messageService: MessageService) {}
 
     getHeroes(): Observable<Hero[]> {
-        // TODO: send the message _after_ fetching the heroes
         this.messageService.add("HeroService: fetched heroes");
-        return of(HEROES);
+        // tslint:disable-next-line:no-magic-numbers
+        return of(HEROES).pipe(delay(1000));
     }
 }
+
+// foo() {
+//     this.getHeroes().subscribe((data) => {
+//         console.log(data);
+//     })
+// }
+
+// async function wait(timeInMs: number): Promise<void> {
+//     setTimeout(() => {
+//         return;
+//     }, timeInMs);
+// }
