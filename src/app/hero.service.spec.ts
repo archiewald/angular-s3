c0580de1d2e08c008d8f9225ba59bccf10c1eef4
archiewald <artur.kozubek1@gmail.com>
2018-09-21 15:48:18 +0200

    add getHeroes() test and make it work

--- src/app/hero.service.spec.ts
+++ src/app/hero.service.spec.ts
@@ -1,10 +1,24 @@
 import {HttpClientTestingModule} from "@angular/common/http/testing";
 import {async, inject, TestBed} from "@angular/core/testing";
+import {of} from "rxjs";
 
+import {Hero} from "./hero";
 import {HeroService} from "./hero.service";
+import {MessageService} from "./message.service";
 
 describe("HeroService", () => {
+    let httpClientSpy: {get: jasmine.Spy};
+    let heroService: HeroService;
+    let messageService: MessageService;
+
     beforeEach(() => {
+        httpClientSpy = jasmine.createSpyObj("HttpClient", ["get"]);
+        messageService = new MessageService();
+        heroService = new HeroService(
+            // tslint:disable-next-line:no-any
+            httpClientSpy as any,
+            messageService
+        );
         TestBed.configureTestingModule({
             providers: [HeroService],
         });
@@ -19,4 +33,18 @@ describe("HeroService", () => {
     it("should be created", inject([HeroService], (service: HeroService) => {
         expect(service).toBeTruthy();
     }));
+
+    it("should return testHeroes on getHeroes()", () => {
+        const testHeroes: Hero[] = [
+            {id: 1, name: "Archie"},
+            {id: 2, name: "Ziomek"},
+        ];
+
+        httpClientSpy.get.and.returnValue(of(testHeroes));
+
+        heroService
+            .getHeroes()
+            .subscribe(heroes => expect(heroes).toEqual(testHeroes), fail);
+        expect(httpClientSpy.get.calls.count()).toBe(1, "one call");
+    });
 });
